fc4c4e4f008f52e65ed9e04b213dfc07875ba17a
Amanmohane11 <164287485+Amanmohane11@users.noreply.github.com>
2025-08-19 10:15:45 -0400

    Fix: add Client as normal folder instead of submodule

---
+++ Client/src/main.jsx
@@ -0,0 +1,11 @@
+Remove-Item -Recurse -Force "Client\.git"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
