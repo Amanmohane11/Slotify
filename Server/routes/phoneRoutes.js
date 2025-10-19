b6eb5ae2f4baaa2b071d371476be23cd5e59cf79
Amanmohane11 <164287485+Amanmohane11@users.noreply.github.com>
2025-10-19 23:38:37 +0530

    Delete Server directory

--- Server/routes/phoneRoutes.js
+++
@@ -1,7 +0,0 @@
-import {Router } from "express";
-import {verifyPhone} from "../controllers/phoneController.js"
-const phoneRouter=Router();
-
-phoneRouter.post("/verify",verifyPhone);
-
-export default phoneRouter;
